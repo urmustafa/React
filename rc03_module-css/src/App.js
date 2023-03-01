86a2a51f4fe2cdaf819d77eff32f418a0292c2e3
Mustafa Tayyar <63878104+urmustafa@users.noreply.github.com>
2023-03-01 14:54:39 +0300

    Add files via upload

---
+++ rc03_module-css/src/App.js
@@ -0,0 +1,15 @@
+import Card from "./components/card/Card"
+import data from "./utils/data"
+
+function App() {
+  console.log(data)
+  return (
+    <>
+      {data.map(({ id, language, img, btnName }) => (
+        <Card key={id} lang={language} img={img} btn={btnName} />
+      ))}
+    </>
+  )
+}
+
+export default App
